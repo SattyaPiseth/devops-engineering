f939fd2d2ea0d0d8610cac6f71a80ec347b67733
MuyleangIng <muyleanging@gmail.com>
2024-04-01 10:22:50 +0000

    update

--- shellscript/createfolder.sh
+++ shellscript/createfolder.sh
@@ -2,13 +2,13 @@
 
 # Define the directory name
 DIR_NAME="MyFolder"
-
+cd /root/shellscript
 # Create the directory if it doesn't already exist
 if [ ! -d "$DIR_NAME" ]; then
-    mkdir "/root/shellscript/$DIR_NAME"
+    mkdir "shellscript/$DIR_NAME"
 fi
 
 # Create or touch the file inside the directory
-touch "/root/shellscript/$DIR_NAME/file.txt"
+touch "$DIR_NAME/file.txt"
 
 echo "Directory and file have been created."
